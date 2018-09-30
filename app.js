3d60d2a0d644cd7992fd2dbbacbd0a3bddfa803c
kwmr1 <40320050+kwmr1@users.noreply.github.com>
2018-09-30 22:50:58 +0900

    練習問題：配列で渡された整数を全て掛けあわせて返すmulti関数を定義

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+function multi(numbers) {
+    let result = numbers[0];    
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+module.exports = {
+    multi: multi
+};
